f83e108c2e1e167b3df7bd56d6beb7c1ab466392
darcyp <eelakoopfm@gmail.com>
2023-02-10 10:37:31 +0200

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
     public static void task1() {
@@ -15,6 +16,7 @@ public class Main {
 
     public static void task2() {
         System.out.println("Задача 2");
-
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(fullName.toUpperCase());
     }
 }
